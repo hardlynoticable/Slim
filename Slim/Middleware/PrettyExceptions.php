4bc6e831fceac956cad1eba24f41f6d3ea5fd1c2
Stephen Akins <stephenakins@gmail.com>
2014-09-25 07:17:50 -0700

    Update PrettyExceptions.php
    Fixed the str_replace newline issue, added an additional newline to cause the last trace div to close, took out the PRE tags (no longer needed), added some further formatting and the DOCTYPE

--- Slim/Middleware/PrettyExceptions.php
+++ Slim/Middleware/PrettyExceptions.php
@@ -89,28 +89,32 @@ class PrettyExceptions extends \Slim\Middleware
         $message = $exception->getMessage();
         $file = $exception->getFile();
         $line = $exception->getLine();
-        $trace = str_replace(array('#', '\n'), array('<div>#', '</div>'), $exception->getTraceAsString());
-        $html = sprintf('<h1>%s</h1>', $title);
-        $html .= '<p>The application could not run because of the following error:</p>';
-        $html .= '<h2>Details</h2>';
-        $html .= sprintf('<div><strong>Type:</strong> %s</div>', get_class($exception));
+        $trace = str_replace(
+                array("#", "\n"),
+                array("<div>#", "</div>\n"),
+                $exception->getTraceAsString() . "\n"
+            );
+        $html = sprintf("<h1>%s</h1>", $title);
+        $html .= "<p>The application could not run because of the following error:</p>";
+        $html .= "<h2>Details</h2>";
+        $html .= sprintf("<div><strong>Type:</strong> %s</div>", get_class($exception));
         if ($code) {
-            $html .= sprintf('<div><strong>Code:</strong> %s</div>', $code);
+            $html .= sprintf("<div><strong>Code:</strong> %s</div>\n", $code);
         }
         if ($message) {
-            $html .= sprintf('<div><strong>Message:</strong> %s</div>', $message);
+            $html .= sprintf("<div style=\"color: #CC3333\"><strong>Message:</strong> %s</div>\n", $message);
         }
         if ($file) {
-            $html .= sprintf('<div><strong>File:</strong> %s</div>', $file);
+            $html .= sprintf("<div><strong>File:</strong> %s</div>\n", $file);
         }
         if ($line) {
-            $html .= sprintf('<div><strong>Line:</strong> %s</div>', $line);
+            $html .= sprintf("<div><strong>Line:</strong> %s</div>\n", $line);
         }
         if ($trace) {
-            $html .= '<h2>Trace</h2>';
-            $html .= sprintf('<pre>%s</pre>', $trace);
+            $html .= "<h2>Trace</h2>\n";
+            $html .= sprintf("%s\n", $trace);
         }
 
-        return sprintf("<html><head><title>%s</title><style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{display:inline-block;width:65px;}</style></head><body>%s</body></html>", $title, $html);
+        return sprintf("<!DOCTYPE html><html>\n<head>\n<title>%s</title>\n<style>body{margin:0;padding:30px;font:12px/1.5 Helvetica,Arial,Verdana,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}strong{display:inline-block;width:65px;}</style>\n</head>\n<body>\n\n%s\n\n</body>\n</html>", $title, $html);
     }
 }
